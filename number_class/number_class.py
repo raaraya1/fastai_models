c3ecb6655dad50ea361940c1967c9be4c869d3e5
raaraya1 <raaraya1@miuandes.cl>
2021-11-14 17:11:31 -0300

    correct number classification model

--- number_class/number_class.py
+++ number_class/number_class.py
@@ -20,6 +20,34 @@ class number_class_st():
         pass
 
     def model(self):
+
+        st.write('''
+        ## Clasificador de dígitos
+
+        Similar al resto de los modelos de visión ya presentados, este modelo busca
+        clasificar imágenes de números entre el 0 al 9. Para esto también se utiliza
+        un modelo pre-entrenado de red convolucional, ajustando las ultimas capas
+        a los dígitos.
+
+        **Nota**:
+        A diferencia del modelo trabajado en el curso (clasificador de 3 y 7), este
+        fue elaborado con un set de datos más completo del banco de imágenes (MNIST).
+
+        ### Datos
+
+        Los datos fueron extraídos del siguiente enlace: http://yann.lecun.com/exdb/mnist/
+        Estos se acceden desde **URLs.MNIST**
+
+        ### Modelo
+
+        **resnet18**: Red neuronal convolucional pre-entrenada extraída desde:
+        https://download.pytorch.org/models/resnet18-f37072fd.pth
+
+        ### DEMO
+
+
+        ''')
+
         #custom()
         # para cargar el modelo
         plt = platform.system()
@@ -37,20 +65,23 @@ class number_class_st():
         learn = load_learner(path)
 
         # para hacer el dibujo del numero
-        st.write('Dibuja un numero del 0 al 9')
+        st.write('**Dibuja un numero del 0 al 9**')
         stroke_width = 7
         stroke_color = "rgb(255, 255, 255)"
         bg_color = "rgb(0, 0, 0)"
-        canvas_result = st_canvas(
-            fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-            stroke_width=stroke_width,
-            stroke_color=stroke_color,
-            background_color=bg_color,
-            update_streamlit='True',
-            height=150,
-            width=150,
-            drawing_mode="freedraw",
-        )
+
+        col1, col2, col3 = st.columns(3)
+        with col2:
+            canvas_result = st_canvas(
+                fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+                stroke_width=stroke_width,
+                stroke_color=stroke_color,
+                background_color=bg_color,
+                update_streamlit='True',
+                height=150,
+                width=150,
+                drawing_mode="freedraw",
+            )
 
         if canvas_result.image_data is not None:
             img = canvas_result.image_data
@@ -59,12 +90,12 @@ class number_class_st():
             im = Image.fromarray(img.astype("uint8"), mode="RGBA")
             im = im.convert('RGB').resize(size=(32, 32))
             #st.write(type(im))
-            prediccion = st.button('prediccion')
+            prediccion = col2.button('Predicción')
             if prediccion:
                 im = PILImage(im)
                 prediccion = learn.predict(im)
                 prob = int(np.round(torch.max(prediccion[2])*100, 0))
                 st.write(f'''
-                Se predice que el numero es el {prediccion[0]} con una probabilidad del {prob}%
+                Se predice que el numero es el **{prediccion[0]}** con una probabilidad del **{prob}%**
                 ''')
                 prediccion = False
