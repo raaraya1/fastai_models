60977f9d939f9fbc1359e4e68837eb542e70b0fb
raaraya1 <raaraya1@miuandes.cl>
2021-11-15 18:11:11 -0300

    fix recomendation model

--- recomendation_system/recomendation_system.py
+++ recomendation_system/recomendation_system.py
@@ -22,6 +22,25 @@ class recomendation_system_st():
         pass
 
     def model(self):
+
+        st.write('''
+        ## Sistema de recomendaciones
+        Este modelo tiene por objetivo predecir la calificación que pondría un
+        usuario sobre algún ítem. De esta manera, para trabajar con este modelo
+        se requiere que los datos contengan:
+        - 1. **Los Ítems** (Ej: Películas)
+        - 2. **Los Usuarios**
+        - 3. **Las Calificaciones** (Ej: Puntuaciones de 0 a 5)
+
+        Para el caso analizado en el curso se plantea elaborar un modelo capaz de
+        'rellenar' las calificaciones faltantes de los usuarios que aún no han visto
+        alguna película. Así, se espera que el modelo sea capaz de
+        ordenar esta información desorganizada bajo una cierta cantidad de categorías
+        o factores. En el curso, a estas categorías le son llamadas **factores latentes**,
+        así al conjunto de todos estos factores latentes (Películas + Usuarios) forman
+        la **Matriz de Embeddings.**
+
+        ''')
         path_movies = untar_data(URLs.ML_100k)
         movies = pd.read_csv(path_movies/'u.item',  delimiter='|', encoding='latin-1',
             usecols=(0,1), names=('movie','title'), header=None)
@@ -46,18 +65,44 @@ class recomendation_system_st():
 
         # datos
         st.write('''
-        # datos
+        ### Datos
+        Los datos fueron extraídos de los datasets que tiene almacenos fastai.
+        Este se accede desde **URLs.ML_100k**
+
+        Aquí se presenta una muestra de cómo se ordenan los datos.
+
+
         ''')
         df_ratings = pd.DataFrame(ratings)
         st.dataframe(df_ratings)
 
 
         st.write('''
-        # Resultado
+        ### Modelo
+
+        En esta ocasión, el modelo utilizado no es el de una red neuronal, sino uno
+        que trabaja sobre el cruce entre los fatores latentes de los usuarios con el de
+        las películas para así generar las puntuaciones. Este modelo se
+        introduce en el curso con la clase **DotProductBias**.
+
+        **Nota: **
+        El modelo generado trabaja sobre 50 factores latentes tanto para los usuarios
+        como para las películas. (también podemos usar la función **get_emb_sz** para
+        calcular el número de factores a utilizar)
+
+        ### DEMO
+
+        Quizás, más interesante que observar las predicciones sobre las puntuaciones
+        de los usuarios, puede ser observar como los títulos de las películas son ordenados en función de los factores latentes.
+
+        El siguiente grafico muestra, de manera tridimensional, como estos títulos
+        son agrupados. Así, aquellos títulos que se encuentren más cercanos corresponden
+        a aquellas películas que comparten mas factores latentes en común.
+
         ''')
 
         num_movies = 1000
-        movies_show = 20
+        movies_show = int(st.sidebar.number_input('Cantidad de películas para mostrar', value=20, min_value=1, max_value=1000))
         g = ratings.groupby('title')['rating'].count() #agrupar por rating
         top_movies = g.sort_values(ascending=False).index.values[:num_movies] # ordenar por rating descendente
         top_idxs = ([learn.dls.classes['title'].o2i[m] for m in top_movies]) # buscar el indice de las peliculas anteriores
@@ -77,3 +122,28 @@ class recomendation_system_st():
 
         fig = px.scatter_3d(df, x='x', y='y', z='z', color='movie')
         st.plotly_chart(fig, use_container_width=True)
+
+        # Recomendacion en base a una pelicula
+        st.write('''
+
+        ### Recomendaciones
+        De esta manera, lo siguiente que podríamos hacer es generar la recomendación de una película
+        en base a un título que demos como input.
+
+        ''')
+
+        cant_recom = int(st.sidebar.number_input('Cantidad de películas para recomendar',
+                                                value=1,
+                                                min_value=1,
+                                                max_value=movies_show-2))
+        movie_input = st.selectbox('', options=movies)
+        movie_factors = learn.model.i_weight.weight
+        idx = learn.dls.classes['title'].o2i[movie_input]
+        distances = nn.CosineSimilarity(dim=1)(movie_factors, movie_factors[idx][None])
+        idx = distances.argsort(descending=True)[1:cant_recom+1]
+        movie_recomend = learn.dls.classes['title'][idx]
+        movie_recomend = pd.DataFrame([i for i in movie_recomend], columns=['Películas Recomendadas'])
+
+        st.write('**Se recomiendan ver las siguientes películas:**')
+        col1, col2, col3 = st.columns(3)
+        col2.table(movie_recomend)
