3f693f47bab515b4d5e4e70643c3b84720e97713
raaraya1 <83246056+raaraya1@users.noreply.github.com>
2023-12-16 22:06:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -78,35 +78,5 @@ def fastai_models():
         tabular_bluebook_st().model()
 
 
-
-# agregar google analytics
-anlytcs_code = """<script async src="https://www.googletagmanager.com/gtag/js?id=UA-210353274-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-210353274-2');
-</script>"""
-
-
-# Fetch the path of the index.html file
-path_ind = os.path.dirname(st.__file__)+'/static/index.html'
-
-# Open the file
-with open(path_ind, 'r') as index_file:
-    data=index_file.read()
-
-    # Check whether there is GA script
-    if len(re.findall('UA-', data))==0:
-
-        # Insert Script for Google Analytics
-        with open(path_ind, 'w') as index_file_f:
-
-            # The Google Analytics script should be pasted in the header of the HTML file
-            newdata=re.sub('<head>','<head>'+anlytcs_code,data)
-
-            index_file_f.write(newdata)
-
-
 if __name__ == '__main__':
     fastai_models()
